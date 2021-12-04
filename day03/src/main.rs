e9a2e14a703d898e11827df710edeb16cfca615c
Mathieu Poumeyrol <kali@zoy.org>
2021-12-04 16:22:02 +0100

    day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,31 @@
+use itertools::Itertools;
+
+fn tri(a: usize, b: usize, c: usize) -> bool {
+    let hypo = a.max(b).max(c);
+    a + b + c - hypo > hypo
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input").unwrap();
+    let lines: Vec<(usize, usize, usize)> = input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .collect();
+    let part1 = lines.iter().filter(|(a, b, c)| tri(*a, *b, *c)).count();
+    dbg!(part1);
+    let part2:usize = lines
+        .iter()
+        .chunks(3)
+        .into_iter()
+        .map(|chunks| {
+            let (a,b,c) = chunks.collect_tuple().unwrap();
+            tri(a.0, b.0, c.0) as usize + tri(a.1, b.1, c.1) as usize + tri(a.2, b.2, c.2) as usize
+        })
+        .sum();
+    dbg!(part2);
 }
