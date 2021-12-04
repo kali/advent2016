e32fb7d572a9eed84a71acd8793fc56a5f65f869
Mathieu Poumeyrol <kali@zoy.org>
2021-12-04 13:27:35 +0100

    day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input").unwrap();
+    let mut pos = num_complex::Complex::new(0i64, 0);
+    let mut dir = num_complex::Complex::new(1, 0);
+    let i = num_complex::Complex::new(0, 1);
+    for step in input.trim().split(", ") {
+        let turn = step.chars().nth(0).unwrap();
+        dir = dir * if turn == 'R' { i } else { i.conj() };
+        pos += dir * step.trim_start_matches(char::is_alphabetic).parse::<i64>().unwrap();
+    }
+    dbg!(pos.im + pos.re);
+    let mut pos = num_complex::Complex::new(0i64, 0);
+    let mut been = std::collections::HashSet::new();
+    'top: for step in input.trim().split(", ") {
+        let turn = step.chars().nth(0).unwrap();
+        dir = dir * if turn == 'R' { i } else { i.conj() };
+        for _ in 0..step.trim_start_matches(char::is_alphabetic).parse::<i64>().unwrap() {
+            pos += dir;
+            if been.contains(&pos) {
+                break 'top;
+            }
+            been.insert(pos);
+        }
+    }
+    dbg!(pos.im + pos.re);
 }
